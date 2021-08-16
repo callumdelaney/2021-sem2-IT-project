68b0164ee69e3a60162250a20aa51616f2d37b01
djgannon <81336295+djgannon@users.noreply.github.com>
2021-08-17 08:18:53 +1000

    added structural folders (models, controllers, routes, views)

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
+require('./models/db.js')
+
+app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
