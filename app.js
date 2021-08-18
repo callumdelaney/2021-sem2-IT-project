2e3bc05941bc7bf1096c7c5eea8b6908a0736890
Wlzrad <callumddelaney@gmail.com>
2021-08-18 11:42:05 +1000

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "/client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 require("./models/db.js");
 // testing /api for client to communicate with server
@@ -17,7 +17,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
