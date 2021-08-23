73df6eeec5c4a19dc154a7ef3fdd2861eb8afc87
djgannon <81336295+djgannon@users.noreply.github.com>
2021-08-23 16:12:11 +1000

    Implement contact adding

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,6 +14,30 @@ const getOneContact = (req, res) => {
    
 }
 
+const addNewContact = (req, res) => {
+    try {
+        const newContact = await Contact.create({
+            "contactId" : req.body.contact_id,
+            "firstName" : req.body.first_name,
+            "lastName" : req.body.last_name,
+            "phone" : req.body.phone,
+            "email" : req.body.email,
+            "category" : req.body.category
+        })
+    
+        new Contact(newContact).save()
+
+        window.alert("Contact created")
+        
+        res.redirect("/contacts")
+    
+    } catch (err) {
+        res.send("Failed")
+        throw(err)
+    }
+  
+}
+
 module.exports = {
     getLogin,
     getContacts,
--- models/contact.js
+++ models/contact.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const contactSchema = new mongoose.Schema({
     contactId : {type : String, required : true, unique : true},
-    first_name : {type : String, required : true},
-    last_name : {type : String, required : true},
+    firstName : {type : String, required : true},
+    lastName : {type : String, required : true},
     phone : {type : String, required : false},
     email : {type : String, required : false},
     notes : [{type : String, required : false}],
