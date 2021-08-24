80437083982bafc816db431379682f3130f7982d
Z4m4niB <zamanib@student.unimelb.edu.au>
2021-08-24 17:20:27 +1000

    CRM-44
    Imported user schema into controller file.
    
    Changed variable names to comply with team coding convention.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
 
-const Contact = mongoose.model("Contact")
+const contact = mongoose.model("Contact")
+const user = mongoose.model("User")
 
 const getLogin = (req, res) => {
     res.send("Hello");
 }
 
 const getContacts = (req, res) => {
-   
+
 }
 
 const getOneContact = (req, res) => {
-   
+
 }
 
 module.exports = {
