30cc10043be6cac69a9a67a53d0f93953427389c
ayda zhao <aydaz@student.unimelb.edu.au>
2021-09-07 05:10:29 +1000

    made tag schema

---
+++ models/tag.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const tagSchema = new mongoose.Schema({
+    //userID required since tags are linked to the users that create them
+    userId : {type : String, required : true, unique : true},
+    tagText : {type : String, required : true},
+    //tag colour stored as string for Hex value
+    tagColour : {type : String, required : true},
+})
+
+const TAG = mongoose.model('Tag', tagSchema)
+
+module.exports = CONTACT
