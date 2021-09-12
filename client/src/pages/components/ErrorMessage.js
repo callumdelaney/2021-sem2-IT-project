637481e91d5833be30e477e66e214f1d708cc8fd
Wlzrad <callumddelaney@gmail.com>
2021-09-12 14:48:41 +1000

    filled in rest of statusCodes

--- client/src/pages/components/ErrorMessage.js
+++ client/src/pages/components/ErrorMessage.js
@@ -1,14 +1,20 @@
 import React from "react";
 import errorIcon from "../../images/error.png";
-
+// returns a custom error message set by the statusCode prop
 function ErrorMessage(props) {
   const code = props.statusCode;
   var text = "";
-
+  // different possible codes
   if (code === 11) {
     text = "email address not found";
+  } else if (code === 12) {
+    text = "incorrect email or password";
+  } else if (code === 13) {
+    text = "email already exists";
+  } else if (code === 14) {
+    text = "password is too short (must be at least 5 characters)";
   }
-
+  // return error if status is not a SUCCESS
   if (code !== 1) {
     return (
       <div className="errorMessage">
