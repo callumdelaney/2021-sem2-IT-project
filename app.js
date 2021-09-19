c328503e2d638ab2a8d1f651b3b16a6882576524
djgannon <81336295+djgannon@users.noreply.github.com>
2021-09-19 20:12:11 +1000

    Implement cors for api endpoint protection, moved endpoints to route.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 const router = require("./routes/route");
-const controller = require("./controllers/controller");
+
+app.use("/", router);
 
 // this code allows retrieval of json files from client-side
 app.use(
@@ -21,21 +22,6 @@ app.use(
 );
 app.use(express.json());
 ///////////////////////////////
-
-// testing /api for client to communicate with server
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-// .post method receives data 'posted' by client-side
-app.post("/api", controller.getLogin);
-
-/////////////////////////////////////////////// added by Callum
-
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const cors = require("cors");
+var corsOptions = {origin: 'https://duckroll-crm.herokuapp.com/contacts'}
 
 const router = express.Router();
 
 const controller = require("../controllers/controller");
 
-router.get("/", async (req, res) => {
-  res.redirect("/login");
+router.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
-router.get("/login", controller.getLogin);
-router.get("/contacts", controller.getContacts);
-router.get("/contacts/:contact", controller.getOneContact);
+// .post method receives data 'posted' by client-side
+router.post("/api/login", controller.login);
+//router.post("/api/signup", controller.newUser);
+router.get("/api/getContacts", cors(corsOptions), controller.getContacts);
+router.get("/api/getOneContact", cors(corsOptions), controller.getOneContact);
+router.post("/api/addContact", cors(corsOptions), controller.addNewContact);
+router.post("/api/editContact", cors(corsOptions), controller.editContact);
+router.post("/api/deleteContact", cors(corsOptions), controller.deleteContact);
+router.post("/api/addNote", cors(corsOptions), controller.addNote);
+router.post("/api/changeCategory", cors(corsOptions), controller.changeCategory);
+
+
+/////////////////////////////////////////////// added by Callum
+
+// All other GET requests not handled before will return our React app
+router.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 
 module.exports = router;
