799aff67852d11e34268d71c0bb86f2b5cdc53ee
a779649182 <fujf1@student.unimelb.edu.au>
2021-10-10 20:44:51 +1100

    Fixed "exceeded maximum stack" error and reimplemented colorpicker in dialog

--- client/src/pages/components/ColorPicker.js
+++ client/src/pages/components/ColorPicker.js
@@ -5,49 +5,49 @@ import { Popover } from "@mui/material";
 
 // Component for color pallet when creating tags
 function ColorPicker(props) {
-	const [anchor, setAnchor] = useState(null);
-	const [color, setColor] = useState(props.color);
-	const [opacity, setOpacity] = useState(props.opacity);
-	const [bg, setBg] = useState(props.bg);
+    const [anchor, setAnchor] = useState(null);
+    const [color, setColor] = useState(props.color);
+    const [opacity, setOpacity] = useState(props.opacity);
+    const [bg, setBg] = useState(props.bg);
 
-	const openPopover = (e) => {
-		setAnchor(e.currentTarget);
-	};
-	const handleChange = (data) => {
-		// color has the hex code, bg(background) has the hsl code, opacity has the opacity value
-		setColor(data.hex);
-		setBg(data.hsl);
-		setOpacity(data.hsl.a);
-	};
+    const openPopover = (e) => {
+        setAnchor(e.currentTarget);
+    };
+    const handleChange = (data) => {
+        // color has the hex code, bg(background) has the hsl code, opacity has the opacity value
+        setColor(data.hex);
+        setBg(data.hsl);
+        setOpacity(data.hsl.a);
+        props.callBack(color, opacity, bg);
+    };
 
-	//console.log(color);
-	return (
-		<div>
-			{/* Color Pallet included in popover, so it does not take space in the tool bar */}
-			<button
-				onClick={openPopover}
-				style={{ backgroundColor: props.color }}
-			>
-				Pick Color
-			</button>
-			<Popover
-				open={Boolean(anchor)}
-				anchorEl={anchor}
-				anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-				transformOrigin={{ vertical: "top", horizontal: "left" }}
-				onClose={() => {
-					setAnchor(null);
-
-					props.callBack(color, opacity, bg);
-				}}
-			>
-				<ChromePicker
-					color={bg}
-					onChangeComplete={handleChange}
-				></ChromePicker>
-			</Popover>
-		</div>
-	);
+    //console.log(color);
+    return (
+        <div>
+            {/* Color Pallet included in popover, so it does not take space in the tool bar */}
+            {/* <button
+                // onClick={() => props.callBack(color, opacity, bg)}
+                style={{ borderColor: props.color, borderRadius: "2rem" }}
+            >
+                Pick Color
+            </button> */}
+            <div
+                style={{
+                    backgroundColor: props.color,
+                    height: "1rem",
+                    width: "20%",
+                    padding: "2px",
+                    margin: "2%",
+                    border: "3px solid grey",
+                }}
+            />
+            <ChromePicker
+                color={bg}
+                onChange={(color) => handleChange(color)}
+            ></ChromePicker>
+            {/* </Popover> */}
+        </div>
+    );
 }
 
 export default ColorPicker;
--- client/src/pages/components/TagCreatorDialog.js
+++ client/src/pages/components/TagCreatorDialog.js
@@ -3,44 +3,64 @@ import { Dialog, DialogTitle } from "@mui/material";
 import ColorPicker from "./ColorPicker";
 
 function TagCreatorDialog(props) {
-	// useState hooks for color picker
-	const [color, setColor] = useState("#fff");
-	const [opacity, setOpacity] = useState("1");
-	const [bg, setBg] = useState({
-		h: 250,
-		s: 0,
-		l: 0.2,
-		a: 1,
-	});
-	const { onClose, open } = props;
+    // useState hooks for color picker
+    const [color, setColor] = useState("#fff");
+    const [opacity, setOpacity] = useState("1");
+    const [bg, setBg] = useState({
+        h: 250,
+        s: 0,
+        l: 0.2,
+        a: 1,
+    });
+    const { onClose, open } = props;
 
-	// callback function to obtain color values from color picker
-	const handleCallBack = (color, opacity, bg) => {
-		setColor(color);
-		setOpacity(opacity);
-		setBg(bg);
-		console.log(bg);
-		console.log(color);
-		console.log(opacity);
-	};
+    // callback function to obtain color values from color picker
+    const handleCallBack = (color, opacity, bg) => {
+        setColor(color);
+        setOpacity(opacity);
+        setBg(bg);
+        // console.log(bg);
+        // console.log(color);
+        // console.log(opacity);
+    };
 
-	const handleClose = () => {
-		// add axios call here then redirect to /contacts
-		onClose();
-	};
+    const handleClose = () => {
+        // add axios call here then redirect to /contacts
+        onClose();
+    };
 
-	return (
-		<Dialog onClose={handleClose} open={open} disableEnforceFocus>
-			<DialogTitle>Create Tag</DialogTitle>
-			<ColorPicker
-				color={color}
-				opacity={opacity}
-				bg={bg}
-				callBack={handleCallBack}
-			/>
-			<button onClick={handleClose}>close</button>
-		</Dialog>
-	);
+    return (
+        <Dialog onClose={handleClose} open={open} disableEnforceFocus>
+            <DialogTitle>Create Tag</DialogTitle>
+            <ColorPicker
+                color={color}
+                opacity={opacity}
+                bg={bg}
+                callBack={handleCallBack}
+            />
+            <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                }}
+            >
+                <input
+                    style={{
+                        width: "30%",
+                        // display: "block",
+                    }}
+                ></input>
+                <button
+                    style={{
+                        width: "20%",
+                    }}
+                >
+                    create tag
+                </button>
+            </div>
+            <button onClick={handleClose}>close</button>
+        </Dialog>
+    );
 }
 
 export default TagCreatorDialog;
