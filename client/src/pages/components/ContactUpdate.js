9a82529e49f89551fb97d450e8fcc963cafa9f74
a779649182 <fujf1@student.unimelb.edu.au>
2021-10-11 21:30:36 +1100

    Implemented custom photo upload for edit and create contact page

--- client/src/pages/components/ContactUpdate.js
+++ client/src/pages/components/ContactUpdate.js
@@ -6,193 +6,220 @@ import { FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
 import { useGlobalState } from "state-pool";
 
 function ContactUpdate(props) {
-	// set state variables (default to contact details)
-	// eslint-disable-next-line
-	const [info, setInfo] = useGlobalState("contactInfo");
-	const [firstName, setFirstName] = useState(props.firstName);
-	const [lastName, setLastName] = useState(props.lastName);
-	const [email, setEmail] = useState(props.email);
-	const [phoneNumber, setPhoneNumber] = useState(props.phoneNumber);
-	const [category, setCategory] = useState(props.category);
-	const [notes, setNotes] = useState(props.notes);
-	// eslint-disable-next-line
-	const [photo, setPhoto] = useState(props.photo);
-	// eslint-disable-next-line
-	const [status, setStatus] = useState(statusCode.SUCCESS);
+    // set state variables (default to contact details)
+    // eslint-disable-next-line
+    const [info, setInfo] = useGlobalState("contactInfo");
+    const [firstName, setFirstName] = useState(props.firstName);
+    const [lastName, setLastName] = useState(props.lastName);
+    const [email, setEmail] = useState(props.email);
+    const [phoneNumber, setPhoneNumber] = useState(props.phoneNumber);
+    const [category, setCategory] = useState(props.category);
+    const [notes, setNotes] = useState(props.notes);
+    // eslint-disable-next-line
+    const [photo, setPhoto] = useState(props.photo);
+    const [preview, setPreview] = useState(props.photo);
+    // eslint-disable-next-line
+    const [status, setStatus] = useState(statusCode.SUCCESS);
 
-	// toggle state for confirmation popup
-	const [isOpen, setIsOpen] = useState(false);
-	const togglePopup = () => {
-		setIsOpen(!isOpen);
-	};
+    const fileSelectedHandler = (e) => {
+        console.log(e.target.files[0]);
+        if (
+            e.target.files.length > 0 &&
+            e.target.files[0].type.includes("image")
+        ) {
+            setPhoto(e.target.files[0]);
+            setPreview(URL.createObjectURL(e.target.files[0]));
+        }
+        console.log(e.target.files[0].type);
+    };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+    // toggle state for confirmation popup
+    const [isOpen, setIsOpen] = useState(false);
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    };
 
-		var localStatus = statusCode.SUCCESS;
-		setStatus(statusCode.SUCCESS);
-		// contact details
-		var userData = {
-			firstName: firstName,
-			lastName: lastName,
-			email: email,
-			category: category,
-			phoneNumber: phoneNumber,
-			notes: notes,
-		};
-		// use axios to post user data to back end for processing, use
-		// response to test for validity
-		axios
-			.post("/api/editContact", userData)
-			.then((response) => {
-				console.log(response.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-		// since status won't change until the end of this function, need local status
-		// to keep track of the actual value
-		if (localStatus === statusCode.SUCCESS) {
-			togglePopup();
-		}
-	};
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-	// color constants used in styles
-	const businessColor = "orange";
-	const personalColor = "yellow";
+        var localStatus = statusCode.SUCCESS;
+        setStatus(statusCode.SUCCESS);
+        // contact details
+        var userData = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            category: category,
+            phoneNumber: phoneNumber,
+            notes: notes,
+            photo: photo,
+        };
+        // use axios to post user data to back end for processing, use
+        // response to test for validity
+        axios
+            .post("/api/editContact", userData)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        // since status won't change until the end of this function, need local status
+        // to keep track of the actual value
+        if (localStatus === statusCode.SUCCESS) {
+            togglePopup();
+        }
+    };
 
-	return (
-		// Contents of the page, each seperated by a div
-		<article className="contact-article">
-			<h1 className="header">Edit Contact</h1>
-			<form className="contact-form" action="" onSubmit={handleSubmit}>
-				<div className="contact-form-control">
-					<label htmlFor="firstName">First name: </label>
-					<input
-						type="text"
-						id="firstName"
-						name="firstName"
-						required
-						value={firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
-				</div>
-				<div className="contact-form-control">
-					<label htmlFor="lastName">Last name: </label>
-					<input
-						type="text"
-						id="lastName"
-						name="lastName"
-						required
-						value={lastName}
-						onChange={(e) => setLastName(e.target.value)}
-					/>
-				</div>
-				<div className="contact-form-control">
-					<label htmlFor="email">Email: </label>
-					<input
-						type="text"
-						id="email"
-						name="email"
-						required
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</div>
-				<div className="contact-form-control">
-					<label htmlFor="Phone">Phone number: </label>
-					<input
-						type="number"
-						id="phone"
-						name="phone"
-						value={phoneNumber}
-						onChange={(e) => setPhoneNumber(e.target.value)}
-					/>
-				</div>
-				<div className="contact-form-control">
-					<label htmlFor="notes" style={{ paddingBottom: "150px" }}>
-						Notes:{" "}
-					</label>
-					<textarea
-						className="contact-form-notes"
-						name="notes"
-						id="notes"
-						value={notes}
-						onChange={(e) => setNotes(e.target.value)}
-						cols="30"
-						rows="10"
-						placeholder="Write Something..."
-					></textarea>
-				</div>
-				{/* category labels */}
-				<div style={{ marginLeft: "20px" }}>
-					<RadioGroup
-						className="contact-form-category"
-						row
-						onChange={(e) => setCategory(e.target.value)}
-					>
-						<FormControlLabel
-							value="business"
-							control={<Radio style={{ color: businessColor }} />}
-							label={
-								<span style={{ fontSize: "22px" }}>
-									Business
-								</span>
-							}
-							// style={{ fontSize: "22px" }}
-							checked={category === "business"}
-						/>
-						<FormControlLabel
-							value="personal"
-							control={<Radio style={{ color: personalColor }} />}
-							label={
-								<span style={{ fontSize: "22px" }}>
-									Personal
-								</span>
-							}
-							checked={category === "personal"}
-						/>
-					</RadioGroup>
-				</div>
+    // color constants used in styles
+    const businessColor = "orange";
+    const personalColor = "yellow";
 
-				{/* changes contact info onClick */}
-				<button type="submit">Save</button>
-				{/* contact saved popup component */}
-				{isOpen && (
-					<Popup
-						content={
-							<>
-								<h1 className="contact-popup-box">
-									Contact Updated Successfully!
-								</h1>
-								<div className="contact-popup-button">
-									<button
-										className=""
-										onClick={() => {
-											// onclick toggles popup and updates info
-											togglePopup();
-											setInfo({
-												addContact: false,
-												editContact: false,
-												firstName: props.firstName,
-												category: props.category,
-												notes: props.notes,
-												phoneNumber: props.phoneNumber,
-												email: props.email,
-												photo: props.photo,
-											});
-										}}
-									>
-										Close
-									</button>
-								</div>
-							</>
-						}
-					/>
-				)}
-			</form>
-		</article>
-	);
+    return (
+        // Contents of the page, each seperated by a div
+        <article className="contact-article">
+            <h1 className="header">Edit Contact</h1>
+            <form className="contact-form" action="" onSubmit={handleSubmit}>
+                <div className="contact-form-control">
+                    <label htmlFor="firstName">First name: </label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        required
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </div>
+                <div className="contact-form-control">
+                    <label htmlFor="lastName">Last name: </label>
+                    <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        required
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </div>
+                <div className="contact-form-control">
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="contact-form-control">
+                    <label htmlFor="Phone">Phone number: </label>
+                    <input
+                        type="number"
+                        id="phone"
+                        name="phone"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                </div>
+                <div className="contact-form-control">
+                    <label htmlFor="notes" style={{ paddingBottom: "150px" }}>
+                        Notes:{" "}
+                    </label>
+                    <textarea
+                        className="contact-form-notes"
+                        name="notes"
+                        id="notes"
+                        value={notes}
+                        onChange={(e) => setNotes(e.target.value)}
+                        cols="30"
+                        rows="10"
+                        placeholder="Write Something..."
+                    ></textarea>
+                </div>
+                {/* category labels */}
+                <div style={{ marginLeft: "20px" }}>
+                    <RadioGroup
+                        className="contact-form-category"
+                        row
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <FormControlLabel
+                            value="business"
+                            control={<Radio style={{ color: businessColor }} />}
+                            label={
+                                <span style={{ fontSize: "22px" }}>
+                                    Business
+                                </span>
+                            }
+                            // style={{ fontSize: "22px" }}
+                            checked={category === "business"}
+                        />
+                        <FormControlLabel
+                            value="personal"
+                            control={<Radio style={{ color: personalColor }} />}
+                            label={
+                                <span style={{ fontSize: "22px" }}>
+                                    Personal
+                                </span>
+                            }
+                            checked={category === "personal"}
+                        />
+                    </RadioGroup>
+                    {/* div for photo upload */}
+                    <div>
+                        <label htmlFor="photo" style={{ fontSize: "15px" }}>
+                            Upload Photo
+                        </label>
+                        <input type="file" onChange={fileSelectedHandler} />
+                    </div>
+                    <img
+                        src={preview}
+                        alt="contactPhoto"
+                        width="230px"
+                        height="230px"
+                    />
+                </div>
+
+                {/* changes contact info onClick */}
+                <button type="submit">Save</button>
+                {/* contact saved popup component */}
+                {isOpen && (
+                    <Popup
+                        content={
+                            <>
+                                <h1 className="contact-popup-box">
+                                    Contact Updated Successfully!
+                                </h1>
+                                <div className="contact-popup-button">
+                                    <button
+                                        className=""
+                                        onClick={() => {
+                                            // onclick toggles popup and updates info
+                                            togglePopup();
+                                            setInfo({
+                                                addContact: false,
+                                                editContact: false,
+                                                firstName: props.firstName,
+                                                category: props.category,
+                                                notes: props.notes,
+                                                phoneNumber: props.phoneNumber,
+                                                email: props.email,
+                                                photo: props.photo,
+                                            });
+                                        }}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
+                            </>
+                        }
+                    />
+                )}
+            </form>
+        </article>
+    );
 }
 
 export default ContactUpdate;
