e35101efa192569ac09b46cbdde3ae8426fdce5a
Z4m4niB <zamanib@student.unimelb.edu.au>
2021-10-12 16:24:00 +1100

    fixed small typo after merge conflict

--- models/contact.js
+++ models/contact.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 
 const contactSchema = new mongoose.Schema({
 
-    firstName : {type : String, required : true},
-    lastName : {type : String, required : true},
-    phone : {type : String, required : false},
-    email : {type : String, required : false},
-    notes : {type : String, required : false},
-    photo : {type : String, required : false},
+    firstName: {type : String, required : true},
+    lastName: {type : String, required : true},
+    phone: {type : String, required : false},
+    email: {type : String, required : false},
+    notes: {type : String, required : false},
+    photo: {type : String, required : false},
     category : {type : String, required : false},
-    tags: {type: [mongoose.Schema.Types.ObjectId], required: false, ref:'Tag'}
-	  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    tags: {type: [mongoose.Schema.Types.ObjectId], required: false, ref:'Tag'},
+	user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 
 })
 
