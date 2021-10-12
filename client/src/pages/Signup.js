bb9b33f38653f5810a603e4d0ee113ed5c02fbc4
Wlzrad <callumddelaney@gmail.com>
2021-10-12 17:59:35 +1100

    fixed status codes, fixed posting username field instead of email

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -7,171 +7,171 @@ import { Link } from "react-router-dom";
 
 // component for sign up page
 function Signup() {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirm, setPasswordConfirm] = useState("");
-    const [status, setStatus] = useState(statusCode.SUCCESS);
-    // toggle state for confirmation popup
-    const [isOpen, setIsOpen] = useState(false);
-    const togglePopup = () => {
-        setIsOpen(!isOpen);
-    };
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordConfirm, setPasswordConfirm] = useState("");
+	const [status, setStatus] = useState(statusCode.SUCCESS);
+	// toggle state for confirmation popup
+	const [isOpen, setIsOpen] = useState(false);
+	const togglePopup = () => {
+		setIsOpen(!isOpen);
+	};
 
-    // handleSubmit is executed when the submit button is clicked
-    const handleSubmit = (e) => {
-        e.preventDefault();
+	// handleSubmit is executed when the submit button is clicked
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// setError based on feedback from back-end
+		var localStatus = status;
+		// if the two password fields don't match, can't move forward
+		if (password !== passwordConfirm) {
+			// dynamically change border color of these elements to red
+			console.log("went here");
+			document.getElementById("password").style["border-color"] = "red";
+			document.getElementById("passwordConfirm").style["border-color"] =
+				"red";
+			// change status code accordingly
+			localStatus = statusCode.MISMATCHED_PASSWORDS;
+			setStatus(statusCode.MISMATCHED_PASSWORDS);
+			return;
+		}
+		// registration details
+		var userData = {
+			firstName: firstName,
+			lastName: lastName,
+			username: email,
+			password: password,
+			phoneNumber: phoneNumber,
+		};
+		// use axios to post user data to back end for processing, use
+		// response to test for validity
+		axios
+			.post("/api/signup", userData)
+			.then((response) => {
+				console.log(response.data);
+				// check if data was saved successfully
+				localStatus = response.data.status;
+				setStatus(localStatus);
+				// since status won't change until the end of this function, need local status
+				// to keep track of the actual value
+				if (localStatus === statusCode.SUCCESS) {
+					togglePopup();
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		console.log(localStatus);
+	};
 
-        // setError based on feedback from back-end
-        var localStatus = status;
-        // if the two password fields don't match, can't move forward
-        if (password !== passwordConfirm) {
-            // dynamically change border color of these elements to red
-            console.log("went here");
-            document.getElementById("password").style["border-color"] = "red";
-            document.getElementById("passwordConfirm").style["border-color"] =
-                "red";
-            // change status code accordingly
-            localStatus = statusCode.MISMATCHED_PASSWORDS;
-            setStatus(statusCode.MISMATCHED_PASSWORDS);
-            return;
-        }
-        // registration details
-        var userData = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password,
-            phoneNumber: phoneNumber,
-        };
-        // use axios to post user data to back end for processing, use
-        // response to test for validity
-        axios
-            .post("/api/signup", userData)
-            .then((response) => {
-                console.log(response.data);
-                // check if data was saved successfully
-                localStatus = response.data.status;
-                setStatus(localStatus);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        // since status won't change until the end of this function, need local status
-        // to keep track of the actual value
-        if (localStatus === statusCode.SUCCESS) {
-            togglePopup();
-        }
-    };
-
-    return (
-        // Contents of the page, each seperated by a div
-        <article className="articleSignup">
-            <h1 className="header">Sign Up</h1>
-            {/* form div containing all of the required fields */}
-            <form className="form" action="" onSubmit={handleSubmit}>
-                <div className="form-control">
-                    <label htmlFor="firstName">First name:* </label>
-                    <input
-                        type="text"
-                        id="firstName"
-                        name="firstName"
-                        required
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="lastName">Last name:* </label>
-                    <input
-                        type="text"
-                        id="lastName"
-                        name="lastName"
-                        required
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="email">Email:* </label>
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="Phone">Phone number: </label>
-                    <input
-                        type="number"
-                        id="phone"
-                        name="phone"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="password">Password:* </label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        required
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                            // reset border color
-                            document.getElementById("password").style[
-                                "border-color"
-                            ] = "transparent";
-                        }}
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="passwordConfirm">Confirm password:* </label>
-                    <input
-                        type="password"
-                        id="passwordConfirm"
-                        name="passwordConfirm"
-                        required
-                        value={passwordConfirm}
-                        onChange={(e) => {
-                            setPasswordConfirm(e.target.value);
-                            // reset border color
-                            document.getElementById("passwordConfirm").style[
-                                "border-color"
-                            ] = "transparent";
-                        }}
-                    />
-                </div>
-                <button type="submit">Register</button>
-                {/* conditional rendering of error message based on status */}
-                <ErrorMessage statusCode={status} />
-            </form>
-            {/* register confirmation popup component */}
-            {isOpen && (
-                <Popup
-                    content={
-                        <>
-                            <b>Account Created Successfully!</b>
-                            <p>
-                                Thank you for your interest! <br /> you can now
-                                login using your credentials
-                            </p>
-                            <Link to="/login">
-                                <button className="popup-button">Login</button>
-                            </Link>
-                        </>
-                    }
-                />
-            )}
-        </article>
-    );
+	return (
+		// Contents of the page, each seperated by a div
+		<article className="articleSignup">
+			<h1 className="header">Sign Up</h1>
+			{/* form div containing all of the required fields */}
+			<form className="form" action="" onSubmit={handleSubmit}>
+				<div className="form-control">
+					<label htmlFor="firstName">First name:* </label>
+					<input
+						type="text"
+						id="firstName"
+						name="firstName"
+						required
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="lastName">Last name:* </label>
+					<input
+						type="text"
+						id="lastName"
+						name="lastName"
+						required
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="email">Email:* </label>
+					<input
+						type="text"
+						id="email"
+						name="email"
+						required
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="Phone">Phone number: </label>
+					<input
+						type="number"
+						id="phone"
+						name="phone"
+						value={phoneNumber}
+						onChange={(e) => setPhoneNumber(e.target.value)}
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="password">Password:* </label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						required
+						value={password}
+						onChange={(e) => {
+							setPassword(e.target.value);
+							// reset border color
+							document.getElementById("password").style[
+								"border-color"
+							] = "transparent";
+						}}
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="passwordConfirm">Confirm password:* </label>
+					<input
+						type="password"
+						id="passwordConfirm"
+						name="passwordConfirm"
+						required
+						value={passwordConfirm}
+						onChange={(e) => {
+							setPasswordConfirm(e.target.value);
+							// reset border color
+							document.getElementById("passwordConfirm").style[
+								"border-color"
+							] = "transparent";
+						}}
+					/>
+				</div>
+				<button type="submit">Register</button>
+				{/* conditional rendering of error message based on status */}
+				<ErrorMessage statusCode={status} />
+			</form>
+			{/* register confirmation popup component */}
+			{isOpen && (
+				<Popup
+					content={
+						<>
+							<b>Account Created Successfully!</b>
+							<p>
+								Thank you for your interest! <br /> you can now
+								login using your credentials
+							</p>
+							<Link to="/login">
+								<button className="popup-button">Login</button>
+							</Link>
+						</>
+					}
+				/>
+			)}
+		</article>
+	);
 }
 
 export default Signup;
