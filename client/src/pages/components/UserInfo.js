3a2de30b3a9d1edef5041833b0b931a1f0c4e21d
Wlzrad <callumddelaney@gmail.com>
2021-10-14 16:22:34 +1100

    more stylings for contact page

--- client/src/pages/components/UserInfo.js
+++ client/src/pages/components/UserInfo.js
@@ -1,19 +1,38 @@
 import React from "react";
 import { useGlobalState } from "state-pool";
+import ManageAccountsIcon from "@material-ui/icons/AccountCircle";
+import LogoutIcon from "@material-ui/icons/MeetingRoom";
+import { AppBar, Toolbar, Menu } from "@material-ui/core";
 
 function UserInfo() {
 	const [userInfo] = useGlobalState("userInfo");
 	return (
-		<div className="user-info">
-			<img src={userInfo.photo} alt="userphoto" />
-			<h1>
-				Hello, {userInfo.firstName} {userInfo.lastName}
-			</h1>
-			<div>
-				<button>Profile Settings</button>
+		<>
+			<div className="user-info">
+				<img src={userInfo.photo} alt="userphoto" />
+				<h1>
+					Hello, {userInfo.firstName} {userInfo.lastName}
+				</h1>
+				<div>
+					<button>
+						<div style={{ display: "flex" }}>
+							<ManageAccountsIcon
+								style={{ paddingRight: "0.5rem" }}
+							/>
+							Profile Settings
+						</div>
+					</button>
+
+					<button>
+						<div style={{ display: "flex" }}>
+							<LogoutIcon style={{ paddingRight: "0.5rem" }} />
+							Logout
+						</div>
+					</button>
+				</div>
+				<h4>{userInfo.email}</h4>
 			</div>
-			<h4>{userInfo.email}</h4>
-		</div>
+		</>
 	);
 }
 
